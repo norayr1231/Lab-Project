4b5631d212d38c8c8c7bf778389c32a4e9968032
norayr1231 <norayr000@gmial.com>
2022-10-11 18:34:05 +0400

    V1.5

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,6 +1,7 @@
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
+import random
 import sys
 
 #main class with methods needed
@@ -24,8 +25,11 @@ class UI(QMainWindow):
         self.button7=self.findChild(QPushButton, "pushButton_7")
         self.button8=self.findChild(QPushButton, "pushButton_8")
         self.button9=self.findChild(QPushButton, "pushButton_9")
+        self.pve=self.findChild(QCheckBox, "checkBox")
         self.start=self.findChild(QPushButton, "start")
         self.label=self.findChild(QLabel, "label")
+
+
 
 
         #all button clicks
@@ -52,7 +56,17 @@ class UI(QMainWindow):
                         self.button8,
                         self.button9,
                     ]
-
+        self.list_for_ai = [
+                        self.button1,
+                        self.button2,
+                        self.button3,
+                        self.button4,
+                        self.button5,
+                        self.button6,
+                        self.button7,
+                        self.button8,
+                        self.button9,
+                    ]
         #code for UI style
         self.setWindowTitle("Tic-Tac-Toe")
         self.setStyleSheet("background-color: white;")
@@ -61,26 +75,50 @@ class UI(QMainWindow):
         self.start.setStyleSheet('QPushButton {color: black;}')
         for b in self.button_list:
             b.setStyleSheet('QPushButton {border: none;}')
-        
+            b.setFont(QFont('Quicksliver', 36))
+
 
         self.show()
 
     #button triggers this method
     def action(self, btn):
-        btn.setFont(QFont('Quicksliver', 36))
-        if self.counter%2 == 0:
-            btn.setText('X')
-            btn.setStyleSheet('QPushButton {background-color: red; color: white;}')
-            self.label.setText("O's Turn")
+        if self.pve.isChecked() == False:
+            if self.counter%2 == 0:
+                btn.setText('X')
+                btn.setStyleSheet('QPushButton {background-color: red; color: white;}')
+                self.label.setText("O's Turn")
+                btn.setEnabled(False)
+            else:
+                btn.setText('O')
+                btn.setStyleSheet('QPushButton {background-color: blue; color: white;}')
+                self.label.setText("X's Turn")
+                btn.setEnabled(False)
+            self.counter += 1
         else:
-            btn.setText('O')
-            btn.setStyleSheet('QPushButton {background-color: blue; color: white;}')
-            self.label.setText("X's Turn")
+            
+                btn.setText('X')
+                btn.setStyleSheet('QPushButton {background-color: red; color: white;}')
+                self.label.setText("You're playing against AI")
+                btn.setEnabled(False)
+                self.ai()
 
 
-        self.counter += 1
-        btn.setEnabled(False)
+
         self.win()
+
+    def ai(self):
+
+        for b in self.list_for_ai:
+            if b.text() == 'X':
+                self.list_for_ai.remove(b)
+
+        if self.list_for_ai != [] and self.label.text()!= 'X Wins':
+            button = random.choice(self.list_for_ai)
+            button.setText('O')
+            button.setStyleSheet('QPushButton {background-color: blue; color: white;}')
+            button.setEnabled(False)
+            self.list_for_ai.remove(button)
+
     
     #disables all buttons after game ends
     def disableAll(self):
@@ -147,6 +185,17 @@ class UI(QMainWindow):
 
     #clears the field for starting new game
     def reset(self):
+        self.list_for_ai = [
+                        self.button1,
+                        self.button2,
+                        self.button3,
+                        self.button4,
+                        self.button5,
+                        self.button6,
+                        self.button7,
+                        self.button8,
+                        self.button9,
+                    ]
         for b in self.button_list:
             b.setText('')
             b.setEnabled(True)
